0e868bdcf562d9d442fcf6f9e730c69371c31924
Andrey Vlasov <andrew.vlasof@yandex.ru>
2016-05-31 23:54:47 +0300

    VisualStudio2015 C#
    C# project
    DabBot - Voice Assistant for Windows

---
+++ DabBot/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace DabBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(35, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(58, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Debug";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DabBot";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DabBot/Form1.cs
@@ -0,0 +1,230 @@
+﻿ //by Vlasov Andrey (andrew.vlasof@yandex.ru)
+ //Version 0.0.1
+using System;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using Microsoft.Speech.Recognition;
+using System.Globalization;
+using System.Diagnostics;
+
+
+namespace DabBot
+{
+    public partial class Form1 : Form
+    {
+     public   SpeechSynthesizer ss = new SpeechSynthesizer();  //синтез речи
+       public SpeechRecognitionEngine sre; //распознавание речи
+       public bool done = false;
+      public bool speechOn = true; //Включение и отключение распознавания речи
+        public NotifyIcon NI;
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                NI = new NotifyIcon();
+       
+                NI.Visible = true;
+                NI.Icon = this.Icon;
+                ss.SetOutputToDefaultAudioDevice();
+
+                ss.Speak("Система запущена");
+
+                CultureInfo ci = new CultureInfo("ru-ru"); //язык распознавания
+                sre = new SpeechRecognitionEngine(ci);
+                sre.SetInputToDefaultAudioDevice();
+                sre.SpeechRecognized += sre_SpeechRecognized;
+
+                Choices ch_StartStopCommands = new Choices();              //словарь главных команд
+                ch_StartStopCommands.Add("Даб");
+                ch_StartStopCommands.Add("Отключись");
+                ch_StartStopCommands.Add("Очистить");
+                ch_StartStopCommands.Add("Трей");
+               
+                GrammarBuilder gb_StartStop = new GrammarBuilder();
+                gb_StartStop.Append(ch_StartStopCommands);
+                Grammar g_StartStop = new Grammar(gb_StartStop);
+
+                Choices ch_Numbers = new Choices();
+                for (int i = 0; i < 100; i++)
+                {
+                    ch_Numbers.Add(i.ToString());
+                }
+                Choices ch_Func = new Choices();    //словарь функции калькулятора
+                ch_Func.Add("плюс");
+                ch_Func.Add("минус");
+                ch_Func.Add("делить");
+                ch_Func.Add("умножить");
+                GrammarBuilder gb_WhatIsXplusY = new GrammarBuilder();
+
+                gb_WhatIsXplusY.Append(ch_Numbers);
+                gb_WhatIsXplusY.Append(ch_Func);
+                gb_WhatIsXplusY.Append(ch_Numbers);
+                Grammar g_WhatIsXplusY = new Grammar(gb_WhatIsXplusY);
+
+
+                Choices ch_words = new Choices(); //словарь запускаемых программ
+                ch_words.Add("Анрил");
+                ch_words.Add("Юнити");
+                ch_words.Add("Блендер");
+                ch_words.Add("Браузер");
+                ch_words.Add("Стим");
+
+                GrammarBuilder gb_words = new GrammarBuilder();
+                gb_words.Append("Запусти");
+                gb_words.Append(ch_words);
+                Grammar g_words = new Grammar(gb_words);
+
+
+                sre.LoadGrammarAsync(g_StartStop);
+                sre.LoadGrammarAsync(g_WhatIsXplusY);
+                sre.LoadGrammarAsync(g_words);
+                ss.Speak("Даб готов к работе");
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+
+
+
+
+            }
+            catch (Exception ex)    
+            {
+                ss.Speak("Ошибка загрузки " + ex.Message);
+                listBox1.Items.Add("Bot->" + ex.Message);
+
+            }
+        }
+
+
+    void sre_SpeechRecognized(object sender,                  //распознавание
+    SpeechRecognizedEventArgs e)
+        {
+            string txt = e.Result.Text;                        //обработка команд
+            float confidence = e.Result.Confidence;
+            if(checkBox1.Checked==true)
+           listBox1.Items.Add("Recognize->" + txt);
+
+            if (confidence < 0.60) return;
+            if (txt.IndexOf("Даб") >= 0)
+            {
+
+
+                listBox1.Items.Add("Bot->Speech ON");
+                ss.SpeakAsync("Да");
+                speechOn = true;
+                if(listBox1.Items.Count>=10) listBox1.Items.Clear();
+            }
+            if (txt.IndexOf("Отключись") >= 0)
+            {
+                listBox1.Items.Add("Bot->Speech is now OFF");
+                ss.SpeakAsync("Жду");
+                speechOn = false;
+            }
+            if (speechOn == false) return;
+          
+            if (txt.IndexOf("Очистить") >= 0)
+            {
+                listBox1.Items.Clear();
+                ss.SpeakAsync("Готово");
+              
+            }
+            if (txt.IndexOf("Трей") >= 0)
+            {
+                listBox1.Items.Add("Bot->Трей");
+
+                if (NI.Visible == false)
+                {
+                    NI.Visible = true;
+
+                    Hide();
+                } else
+                {
+                    NI.Visible = false;
+                    WindowState = FormWindowState.Normal;
+                    Show();
+                    
+                }
+                ss.SpeakAsync("Готово");
+                
+               
+            }
+
+            if (txt.IndexOf("плюс") >= 0 | txt.IndexOf("минус") >= 0 | txt.IndexOf("разделить") >= 0 | txt.IndexOf("умножить") >= 0)
+            {
+                string[] words = txt.Split(' ');
+                int num1 = int.Parse(words[0]);
+                int num2 = int.Parse(words[2]);
+                int sum=0;
+                if (txt.IndexOf("плюс") >= 0)
+                {
+                     sum = num1 + num2;
+                }
+                else if(txt.IndexOf("минус") >= 0)
+                {
+                     sum = num1 - num2;
+
+                }  else if (txt.IndexOf("умножить") >= 0)
+                {
+                     sum = num1 * num2;
+                }
+                else if (txt.IndexOf("делить") >= 0)
+                {
+                    if (num2 != 0)
+                        sum = num1 / num2;
+                    else ss.SpeakAsync("На ноль нельзя делить");
+                }
+
+                listBox1.Items.Add("Bot->"+ sum );
+                ss.SpeakAsync(sum.ToString());
+            }
+
+            if (txt.IndexOf("Запусти") >= 0 )
+            {
+                string[] words = txt.Split(' ');
+                string num2 = words[1];
+                
+               
+                if (num2== "Анрил")
+                {
+                    Process.Start(@"C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win64\EpicGamesLauncher.exe");
+                }
+                else if (num2 == "Юнити")
+                {
+                    Process.Start(@"C:\Program Files\Unity\Editor\Unity.exe");
+
+                }
+                else if (num2== "Блендер")
+                {
+                    Process.Start(@"C:\Program Files\Blender Foundation\Blender\blender.exe");
+                }
+                else if (num2== "Браузер")
+                {
+                    Process.Start(@"C:\Program Files (x86)\Mozilla Firefox\firefox.exe");
+                }
+                else if (num2 == "Стим")
+                {
+                    Process.Start(@"C:\Program Files (x86)\Steam\Steam.exe");
+                }
+
+                listBox1.Items.Add("Bot->Запустил " + num2);
+                ss.SpeakAsync("Запустил");
+            }
+        } // sre_SpeechRecognized
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+      public  string s = "0";
+  
+    }
+}
